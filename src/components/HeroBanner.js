f14d1ed78703880dfc69b313c32b006bf7f07575
Hannes Widrig <hwidrig@hwidrig.hsd1.in.comcast.net>
2018-06-10 23:51:37 -0400

    Banner progress to properly handle height

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,7 +1,19 @@
 import React from 'react'
 import styled from 'styled-components'
 import HeroBlock from './HeroBlock'
-import HeroBackground from './HeroBackground'
+import heroSVG from './../images/hero.svg'
+// import videoBackground from './../images/test.png'
+
+const Filler = styled.div`
+  height: ${props => props.height}px;
+`
+
+const HeroBackground = styled.img`
+  z-index: -99;
+  max-height: 652px;
+  position: absolute;
+  top: 0px;
+`
 
 const HeroSection = styled.section`
   font-family: ${props => props.theme.fonts.netto};
@@ -10,13 +22,54 @@ const HeroSection = styled.section`
   align-items: center;
   flex-direction: column;
   padding: 0px 20px;
+  position: relative;
 `
 
-const HeroBanner = props => (
-  <HeroSection>
-    <HeroBackground />
-    <HeroBlock />
-  </HeroSection>
-)
+// const VideoBackground = styled.img`
+//   z-index: -1;
+//   position: absolute;
+//   bottom: 0px;
+//   height: 75px;
+// `
+
+class HeroBanner extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      height: 0,
+    }
+    this.updateHeight = this.updateHeight.bind(this)
+  }
+
+  updateHeight() {
+    this.setState({ height: 0 }) // REF IMG
+  }
+
+  componentDidMount() {
+    this.updateHeight()
+    window.addEventListener('load', this.updateHeight)
+    window.addEventListener('resize', this.updateHeight)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('load', this.updateHeight)
+    window.removeEventListener('resize', this.updateHeight)
+  }
+
+  render() {
+    return (
+      <HeroSection>
+        <Filler height={this.state.height} />
+        <HeroBackground
+          className="hero-svg"
+          src={heroSVG}
+          alt="Call to Action image"
+        />
+        <HeroBlock />
+        {/* <VideoBackground src={videoBackground} alt="background image" /> */}
+      </HeroSection>
+    )
+  }
+}
 
 export default HeroBanner
